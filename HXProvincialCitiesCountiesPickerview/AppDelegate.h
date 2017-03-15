3f82ff6375635796bf5a9ac58fbb114ccd894396
赵鹏飞 <13346939591@163.com>
2017-03-15 09:50:12 +0800

    代码提交

---
+++ HXProvincialCitiesCountiesPickerview/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HXProvincialCitiesCountiesPickerview
+//  github:https://github.com/huangxuan518 博客：blog.libuqing.com
+//  Created by 黄轩 on 16/7/8.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HXProvincialCitiesCountiesPickerview/HXViewController.h
@@ -0,0 +1,15 @@
+//
+//  HXViewController.h
+//  HXProvincialCitiesCountiesPickerview
+//  github:https://github.com/huangxuan518 博客：blog.libuqing.com
+//  Created by 黄轩 on 16/7/8.
+//  Copyright © 2016年 黄轩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HXViewController : UIViewController
+
+
+@end
+
